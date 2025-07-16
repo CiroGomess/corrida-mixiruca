a78dedb62cb52f089153bb342d3153ce3c335352
CiroGomess <ciro.ggj@gmail.com>
2025-07-16 14:48:19 -0300

    upt: config de deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,8 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/corridinha",
   optimizeDeps: {
     exclude: ['lucide-react'],
-  },
-
-  build:{
-    outDir: '../Live',
-    emptyOutDir: true,
   }
 });
